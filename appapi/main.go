e46e230b265fd8912af9b7e5961fb88e6ae31f70
theboshy <opd011089>
2018-04-18 15:29:27 -0500

    arch refresh

--- appapi/main.go
+++ appapi/main.go
@@ -66,63 +66,54 @@ func IndexHandler() gin.HandlerFunc {
 	}
 }
 
-// HeapHandler will pass the call from /debug/pprof/heap to pprof
 func HeapHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		pprof.Handler("heap").ServeHTTP(ctx.Writer, ctx.Request)
 	}
 }
 
-// GoroutineHandler will pass the call from /debug/pprof/goroutine to pprof
 func GoroutineHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		pprof.Handler("goroutine").ServeHTTP(ctx.Writer, ctx.Request)
 	}
 }
 
-// BlockHandler will pass the call from /debug/pprof/block to pprof
 func BlockHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		pprof.Handler("block").ServeHTTP(ctx.Writer, ctx.Request)
 	}
 }
 
-// ThreadCreateHandler will pass the call from /debug/pprof/threadcreate to pprof
 func ThreadCreateHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		pprof.Handler("threadcreate").ServeHTTP(ctx.Writer, ctx.Request)
 	}
 }
 
-// CmdlineHandler will pass the call from /debug/pprof/cmdline to pprof
 func CmdlineHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		pprof.Cmdline(ctx.Writer, ctx.Request)
 	}
 }
 
-// ProfileHandler will pass the call from /debug/pprof/profile to pprof
 func ProfileHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		pprof.Profile(ctx.Writer, ctx.Request)
 	}
 }
 
-// SymbolHandler will pass the call from /debug/pprof/symbol to pprof
 func SymbolHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		pprof.Symbol(ctx.Writer, ctx.Request)
 	}
 }
 
-// TraceHandler will pass the call from /debug/pprof/trace to pprof
 func TraceHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		pprof.Trace(ctx.Writer, ctx.Request)
 	}
 }
 
-// MutexHandler will pass the call from /debug/pprof/mutex to pprof
 func MutexHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		pprof.Handler("mutex").ServeHTTP(ctx.Writer, ctx.Request)
