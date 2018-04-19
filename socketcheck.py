25bb8052758ea877bfd8845f64ee81ed9fbeeea3
theboshy <opd011089>
2018-04-19 15:02:52 -0500

    proto lang support and port check

---
+++ socketcheck.py
@@ -0,0 +1,7 @@
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+result = sock.connect_ex(('127.0.0.1',3000))
+if result == 0:
+   print "Port is open"
+else:
+   print "Port is not open"
